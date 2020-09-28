827d8da488a58d7c03999bc01df0e55526738e3e
Sam Lam <trykslam@gmail.com>
2020-09-28 21:24:20 +0200

    Develop lumpvor2d, and normal and tangent vector computation functions
    The template functions are developed using the algorithm discussed in
    Katz and Plotkin 2001.
    
    lumpvor2d compute the velocity at an arbitrary collocation point (xcol, zcol)
    due to vortex element of circulation circvor, placed at (xvor, zvor).
    
    Normal and tangent vector computation based on the panel inclination.

--- potentialSolver/discreteVortexMethod.py
+++ potentialSolver/discreteVortexMethod.py
@@ -1,54 +1,70 @@
 """
 Contains the discrete vortex method for the thin airfoil theory
 """
+
 import numpy as np
+
 
 def lumpvor2d(xcol, zcol, xvor, zvor, circvor=1):
     """
     Compute the velocity at an arbitrary collocation point (xcol, zcol) due to vortex element of circulation circvor,
     placed at (xvor, zvor).
 
-    :param circvor:
-    :param xcol:
-    :param ycol:
-    :param xvor:
-    :param yvor:
+    :param xcol: x-coordinate of the collocation point
+    :param zcol: z-coordinate of the collocation point
+    :param xvor: x-coordinate of the vortex
+    :param zvor: z-coordinate of the vortex
+    :param circvor: circulation strength of the vortex (base units)
 
     :return: 1D array containing the velocity vector (u, w) (x-comp., z-comp.)
     :rtype: ndarray
 
     """
-
     # Compute the radius vector from the collocation point to the vortex element
 
-    # some algebra
+    # transformation matrix for the x, z distance between two points
+    dcm = np.array([[0, 1],
+                    [-1, 0]])
 
-    return NotImplementedError
+    # magnitude of the distance between two points
+    r_vortex_sq = (xcol - xvor) ** 2 + (zcol - zvor) ** 2
+
+    # the distance in x, and z between two points
+    dist_vec = np.array([[xcol - xvor], [zcol - zvor]])
+
+    norm_factor = circvor / (2.0 * np.pi * r_vortex_sq)  # circulation at vortex element / circumferential distance
+
+    # velocity of vortex element on collocation point
+    vel_vor = circvor / (norm_factor * dcm @ dist_vec)
+
+    return vel_vor
 
 
 def normal_vector(alpha_i):
     """
     Compute the normal vector of the vortex element (xvor, zvor) on each panel (at collocation point).
 
-    :param alphai: 1D array of the panel inclination of each panel (computed at the collocation point on each panel)
+    :param alpha_i: 1D array of the panel inclination of each panel (computed at the collocation point on each panel)
 
     :return: 1D array containing the normal vectors at the collocation of each panel
     """
-    return NotImplementedError
+
+    return np.array([np.sin(alpha_i), np.cos(alpha_i)]).T  # transpose because created array is (2, len(alpha_i))
 
 
 def tangent_vector(alpha_i):
     """
     Compute the normal vector of the vortex element (xvor, zvor) on each panel (at collocation point).
 
-    :param alphai: 1D array of the panel inclination of each panel (computed at the collocation point on each panel)
+    :param alpha_i: 1D array of the panel inclination of each panel (computed at the collocation point on each panel)
 
     :return: 1D array containing the tangent vectors at the collocation of each panel
     """
-    return NotImplementedError
+
+    return np.array([np.cos(alpha_i), -np.sin(alpha_i)]).T  # transpose because created array is (2, len(alpha_i))
 
 
-def compute_circulation():
+def compute_circulation(aoa, q_inf, airfoil):
     """
     Compute the circulation at each collocation point
 
@@ -56,6 +72,8 @@ def compute_circulation():
     """
 
     # compute RHS
+    u_inf, w_inf = np.cos(aoa) * q_inf, np.cos(aoa) * q_inf  # compute the free-stream velocity component
+
 
     # compute discretization matrix
 
@@ -63,3 +81,12 @@ def compute_circulation():
 
     return NotImplementedError
 
+
+if __name__ == "__main__":
+    test_array = np.array([
+        [0.0, 0.0, 0.0],
+        [0.01, 0.1, 0.2],
+        [0.01, 0.1, 0.2],
+        [0.01, 0.1, 0.2],
+        [0.01, 0.1, 0.2]
+    ])
