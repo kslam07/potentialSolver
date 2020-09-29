785ab570d9e0311c1e15ef8354e02961aa71165b
ThomasRV <thomas.verduyn@gmail.com>
2020-09-29 19:54:58 +0200

    Add postProcess function to plot panels

--- potentialSolver/postProcess.py
+++ potentialSolver/postProcess.py
@@ -1,3 +1,21 @@
 """
 Functions created for plotting
 """
+from potentialSolver.airfoil import Airfoil
+import matplotlib.pyplot as plt
+
+def plot_airfoil(airfoil):
+    # plot for test
+    xloc, yloc, x_1, y_1, x_3, y_3, alpha = airfoil.datafile
+    plt.scatter(x_1[:-1], y_1[:-1])
+    plt.scatter(x_3[:-1], y_3[:-1])
+    plt.plot(xloc, yloc, marker='x')
+    plt.axis('scaled')
+    # plt.ylim(0,2*max(yloc))
+    plt.grid()
+    plt.show()
+    return
+
+filename='NACA2414.txt'
+data=Airfoil(25,1,'NACA2414.txt')
+plot_airfoil(data)
