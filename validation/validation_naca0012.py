94e4572beb17ec9eb3574bc1e325c9aaa247a6a1
ThomasRV <thomas.verduyn@gmail.com>
2020-12-09 10:06:18 +0100

    Add validation/verification files for naca0012 with Ladson data

--- validation/validation_naca0012.py
+++ validation/validation_naca0012.py
@@ -11,6 +11,13 @@ def reader():
             contents[index] = ' '.join(line.split()).split(' ')
     return contents
 
+data=np.loadtxt(Path(__file__).parent.parent/ 'data' / 'data_ladson.txt')
+dcp=[]
+for i in range(int((len(data)-1)/2)):
+    print(data[i,1],data[len(data)-i-1,1])
+    dcp.append(data[i,1]-data[len(data)-i-1,1])
+dcp.append(data[int((len(data)-1)/2),1])
+
 filename='naca0012.txt'
 x = reader()
 
@@ -31,7 +38,7 @@ for i in range(31, 35):
 
 # print ([i-31 for i in xloc if i>31])
 
-aoa = 4
+aoa = 10.0254
 eps = 0.1
 
 params = {"npanels": 50, "eps": 0.1, "datafile": "naca0015.txt", "airfoil_type": "naca"}
@@ -44,8 +51,10 @@ airfoil=Airfoil(**params)
 airfoil.run(aoa,1)
 xcol = airfoil.datafile[4, :-1]
 
-plt.plot(x_loc, -1 * np.array(pressure[31:]), label='experimental data',marker='x',ls='')
+#plt.plot(x_loc, -1 * np.array(pressure[31:]), label='experimental data',marker='x',ls='')
 plt.plot(xcol, airfoil.results[-1],label='numerical solution')
+plt.plot(data[:23,0],dcp,label='Ladson data')
 plt.xlabel('x/c')
 plt.ylabel(r'$C_p$')
 plt.legend()
+plt.grid()
